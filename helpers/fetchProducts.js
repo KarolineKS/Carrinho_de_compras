1e5e7002c38d5ff01e954b52a77f254e40c23099
Karoline <kkohlsstangherlin@gmail.com>
2022-08-18 00:13:48 -0300

    Arrumando erros do lint requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,17 +1,16 @@
-const fetchProducts = async(produto) => {
-  try { 
-    if(!produto){
-      throw new Error ('You must provide an url')
+const fetchProducts = async (produto) => {
+  try {
+    if (!produto) {
+      throw new Error('You must provide an url');
     }
-  const caminho = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`
-  const resultado = await fetch(caminho)
-  const objeto = await resultado.json()
-  return objeto
-} catch(erro) {
-  return erro
-}
-}
-
+    const caminho = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
+    const resultado = await fetch(caminho);
+    const objeto = await resultado.json();
+    return objeto;
+  } catch (erro) {
+    return erro;
+  }
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
