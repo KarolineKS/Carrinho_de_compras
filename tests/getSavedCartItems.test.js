29be905a508fe93eb603599ffb45d06040a49011
Karoline <kkohlsstangherlin@gmail.com>
2022-08-19 15:46:27 -0300

    Implementando testes para getSavedCartItems e para saveCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -3,7 +3,16 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('4 - Teste a função getSavedCartItems', async() => {
+  it('1 - Verifica se ao executar getSavedCartItems o método localStorage.setItem é chamado', () => {
+    expect.assertions(1);
+    await getSavedCartItems()
+    expect(await localStorage.setItem()).toBeCalled();
+  });
+
+  it('2 - Verifica se ao executar getSavedCartItems o método localStorage.setItem é chamado com `cartItems`', async() => {
+    expect.assertions(1);
+    await getSavedCartItems();
+    expect(await localStorage.setItem()).toHaveBeenCalledWith('cartItems');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -3,7 +3,19 @@ const saveCartItems = require('../helpers/saveCartItems');
 
 localStorageSimulator('setItem');
 
-describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('3 - Teste a função saveCartItems', async() => {
+  it('1 - Verifica se ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    expect.assertions(1);
+    const argumento = <ol><li>Item</li></ol>;
+    await saveCartItems(argumento);
+    expect(await localStorage.setItem()).toBeCalled();
+  });
+
+  it('2 - Verifica se ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro `cartItems` e o segundo sendo o valor passado como argumento para saveCartItems', async() => {
+    expect.assertions(2);
+    const argumento = <ol><li>Item</li></ol>;
+    await saveCartItems(argumento);
+    expect(await localStorage.setItem()).toHaveBeenNthCalledWith(1,'cartItems');
+    expect(await localStorage.setItem()).toHaveBeenNthCalledWith(2, saveCartItems); 
+  });
 });
