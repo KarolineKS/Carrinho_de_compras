5c2245d14c2662a5d5ec2c8a2f47d7386640b79d
Karoline <kkohlsstangherlin@gmail.com>
2022-08-20 15:27:44 -0300

    Requisito 8 - Carregue o carrinho de compras ao iniciar a página

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -7,12 +7,12 @@ describe('4 - Teste a função getSavedCartItems', () => {
   it('1 - Verifica se ao executar getSavedCartItems o método localStorage.setItem é chamado', () => {
     expect.assertions(1);
     getSavedCartItems()
-    expect(localStorage.getItem()).toBeCalled();
+    expect(localStorage.getItem).toBeCalled();
   });
 
   it('2 - Verifica se ao executar getSavedCartItems o método localStorage.setItem é chamado com `cartItems`', () => {
     expect.assertions(1);
     getSavedCartItems();
-    expect(localStorage.getItem()).toHaveBeenCalledWith('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -14,7 +14,6 @@ describe('3 - Teste a função saveCartItems', () => {
   it('2 - Verifica se ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro `cartItems` e o segundo sendo o valor passado como argumento para saveCartItems', () => {
     expect.assertions(1);
     saveCartItems(argumento);
-    expect(localStorage.setItem()).toHaveBeenNthCalledWith(1,'cartItems');
-    expect(localStorage.setItem()).toHaveBeenNthCalledWith(2, saveCartItems); 
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', argumento);
   });
 });
